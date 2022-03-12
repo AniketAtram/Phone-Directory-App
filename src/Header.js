b8fbf11dc0d5a0cedcb1dbc0d90f8e2fb68545bb
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2022-03-12 14:07:57 +0530

    Added and tested inline CSS

--- src/Header.js
+++ src/Header.js
@@ -14,10 +14,19 @@ class Header extends Component {
 }
 */
 
+// Adding the inline CSS props
+const headerStyle = {
+    textAlign: 'center',
+    padding: 20 ,
+    background: '#000',
+    color: '#FFF',
+    textTransform: 'uppercase'
+};
+
 // Creating header as a functional componenet
 const Header = function(){
     return (
-        <div className="header"> Phone Directory </div>
+        <div className="header" style={headerStyle}> Phone Directory </div>
     )
 }
 
